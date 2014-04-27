939b055cc8ef755e07bc5c950172b60693efe4ab
proshkin <proshkin@gmail.com>
2014-04-27 16:40:17 -0700

    Third commit

--- run_analysis.R
+++ run_analysis.R
@@ -1,37 +1,3 @@
-install.packages("data.table")
-library(data.table)
-
-setwd("./Coursera/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset")
-f_features <- "features.txt"
-f_activity <- "activity_labels.txt"
-f_test_x_name <- "./test/X_test.txt"
-f_test_y_name <- "./test/y_test.txt"
-f_test_subj <- "./test/subject_test.txt"
-
-f_train_x_name <- "./train/X_train.txt"
-f_train_y_name <- "./train/y_train.txt"
-f_train_subj <- "./train/subject_train.txt"
-
-#f_test_x <- read.csv(f_test_x_name, sep=" ")
-w <- rep(16, 561)
-y_t<- f_test_y
-x_t <- read.fwf(f_test_x_name, widths=w, buffersize=100)
-y_t <- read.fwf(f_test_y_name, widths=c(1))
-subj <- read.csv(f_test_subj, sep=" ", header=FALSE, stringsAsFactors=FALSE)
-
-x_r <- read.fwf(f_train_x_name, widths=w, buffersize=100)
-y_r <- read.fwf(f_train_y_name, widths=c(1))
-subj_r <- read.csv(f_train_subj, sep=" ", header=FALSE, stringsAsFactors=FALSE)
-
-
-features <- read.csv(f_features, sep=" ", header=FALSE, stringsAsFactors=FALSE)
-activity <- read.csv(f_activity, sep=" ", header=FALSE, stringsAsFactors=FALSE)
-pmatch(features[,2],c("mean","std"))
-pmatch("bc", "abcd")
-g_mean <- grep("mean()",features[,2], fixed=T)
-g_std <- grep("std()",features[,2], fixed=T)
-g_mean_std <- c(g_mean , g_std)
-col_names <- install.packages("data.table")
 library(data.table)
 
 setwd("./Coursera/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset")
